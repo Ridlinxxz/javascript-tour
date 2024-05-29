0c2c0c6c6410a3054994cb3188303aedecf87845
Oladoja Ridwan <horlahitan41@gmail.com>
2024-05-30 00:00:53 +0100

    more

---
+++ lesson 3/script.js
@@ -0,0 +1,7 @@
+// LOOPS is use for repetitive object
+ 
+//'for' loops keeps running while the condition is TRUE
+for(let rep = 1; rep <= 20; rep++) {
+    // console.log('Lifting weights repetition 1');
+    console.log(`Lifting weights repetition ${rep} times.`);
+}
