9e325c7a62b941dffd35509e391fa797b996f267
Oladoja Ridwan <horlahitan41@gmail.com>
2024-05-30 10:39:46 +0100

    more

--- lesson 3/script.js
+++ lesson 3/script.js
@@ -5,3 +5,32 @@ for(let rep = 1; rep <= 20; rep++) {
     // console.log('Lifting weights repetition 1');
     console.log(`Lifting weights repetition ${rep} times.`);
 }
+
+const ridwan = [
+        'Ridwan',
+        'Oladoja',
+        2024 - 1997,
+        'designer',
+        ['Micheal', 'ade', 'shola'],
+        true
+];
+const types = []
+
+
+for (let i = 0; i < ridwan.length; i++) {
+    //Reading from ridwan array
+    console.log(ridwan[i], typeof ridwan[i]);
+
+    //Filling types array
+    types[i] = typeof ridwan[i];
+}
+console.log(types);
+
+//FOR example
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+   ages.push(2037 - years[i]); 
+}
+console.log(ages);
