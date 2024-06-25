33674f96ca755e3926b8d06fbf478409d759026c
Oladoja Ridwan <horlahitan41@gmail.com>
2024-06-25 17:44:29 +0100

    more

--- lesson 3/script.js
+++ lesson 3/script.js
@@ -78,7 +78,7 @@ for (let i = ridwan.length-1; i >= 0; i--) {
 for (let exercise = 1; exercise < 5; exercise++) {
     console.log(`-------Starting exercise ${exercise}`);
 
-    for (let rep = 1; rep < 6; rep++) {
-        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}🏋️‍♂️`);
+    for (let rep = 1; rep < 11; rep++) {
+        console.log(`Exercise ${exercise}: Pump push up ${rep}🏋️‍♂️ times.`);
     }
 }
