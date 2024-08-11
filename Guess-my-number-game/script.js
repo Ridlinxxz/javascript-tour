14f9773eecf93b4e8607d719d200e0408dac7963
Oladoja Ridwan <horlahitan41@gmail.com>
2024-08-11 15:49:28 +0100

    more

---
+++ Guess-my-number-game/script.js
@@ -0,0 +1,22 @@
+'use strict';
+// //DOM MANIPULATION> Document Object Model
+// console.log(document.querySelector('.message').textContent); //calling directly from html
+// document.querySelector('.message').textContent = 'correct Number!'; //manipulaying the html content
+// console.log(document.querySelector('.message').textContent); //calling after manipulation
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+//USING EventListner
+const number = Math.random()
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number';
+  }
+});
