832aa1597cd6ebafed9ca1e0150e1b657a1f7604
Oladoja Ridwan <horlahitan41@gmail.com>
2024-08-11 16:51:19 +0100

    moreee

--- Guess-my-number-game/script.js
+++ Guess-my-number-game/script.js
@@ -10,13 +10,33 @@
 // console.log(document.querySelector('.guess').value);
 
 //USING EventListner
-const number = Math.random(cd)
+const secretNumber = Math.trunc(Math.random() * 20 + 1); //Random Numbers betwwen 1-20
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'No Number';
+    document.querySelector('.message').textContent = '⛔ No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent =
+        '😞 Too High! Try Again.';
+      score--;
+    } else {
+      document.querySelector('.message').textContent = '😟 You lost the game!';
+    }
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = '😞 Too Low! Try Again.';
+      score--;
+    } else {
+      document.querySelector('.message').textContent = '😟 You lost the game!';
+    }
+    document.querySelector('.score').textContent = score;
   }
 });
