65ceb5b4541663aa02a9a861e6c6d0bdfe19894f
Oladoja Ridwan <horlahitan41@gmail.com>
2024-10-02 21:48:06 +0100

    moreee

--- Guess-my-number-game/script.js
+++ Guess-my-number-game/script.js
@@ -1,7 +1,8 @@
 'use strict';
+
 // //DOM MANIPULATION> Document Object Model
 // console.log(document.querySelector('.message').textContent); //calling directly from html
-// document.querySelector('.message').textContent = 'correct Number!'; //manipulaying the html content
+// document.querySelector('.message').textContent = '🏆 correct Number!'; //manipulaTing the html content
 // console.log(document.querySelector('.message').textContent); //calling after manipulation
 // document.querySelector('.number').textContent = 13;
 // document.querySelector('.score').textContent = 10;
@@ -17,28 +18,38 @@ document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
+  //WHEN THERE IS NO INPUT
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number';
-  } else if (guess === secretNumber) {
+  }
+  //WHEN THE PLAYER WINS
+  else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
-  } else if (guess > secretNumber) {
+  
+  document.querySelector('body').style.backgroundColor = #60b347;
+
+  //WHEN GUESS IS TOO HIGH
+  }
+  else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent =
         '😞 Too High! Try Again.';
-      score--;
+      score--; //decreasing the SCORE value by 1
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = '😟 You lost the game!';
-      document.querySelector('.score').textContent = 0;
+      document.querySelector('.score').textContent = 0; //change the score value to 0
     }
-  } else if (guess < secretNumber) {
+  }
+  //WHEN GUESS IS TOO LOW
+  else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '😞 Too Low! Try Again.';
-      score--;
+      score--; //decreasing the SCORE value by 1
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = '😟 You lost the game!';
-      document.querySelector('.score').textContent = 0;
+      document.querySelector('.score').textContent = 0; //change the score value to 0
     }
   }
 });
