0a18d6fcaffb37b6220b027adf3bbc6c695c2dda
Oladoja Ridwan <horlahitan41@gmail.com>
2024-10-23 11:59:26 +0100

    more

---
+++ modal game/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+//compressing showmodal function
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+//FOR LOOP
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', showModal);
+//   btnsOpenModal[i].addEventListener('click', function () {
+//     modal.classList.remove('hidden');
+//     overlay.classList.remove('hidden');
+//   });
+
+//compressing closemodal function
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+btnCloseModal.addEventListener('click', closeModal);
+// btnCloseModal.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+overlay.addEventListener('click', closeModal);
+// overlay.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
